c61fb65323fc73043f1f570ad08bc01f34f2a1e2
shoichinishiguchi <shoichi2228@yahoo.co.jp>
2020-02-16 18:41:01 +0900

    feat: 10-1-4

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,27 @@
 Rails.application.routes.draw do
+  config = Rails.application.config.baukis2
 
-  namespace :staff do
-    root "top#index"
-    get "login" => "sessions#new", as: :login
-    post "session" => "sessions#create", as: :session
-    delete "session" =>  "sessions#destroy"
+  constraints host: config[:staff][:host] do
+    namespace :staff, path: config[:staff][:path] do
+      root "top#index"
+      get "login" => "sessions#new", as: :login
+      resource :session, only: [ :craete, :destroy ]
+      resource :account, except: [ :new, :create, :destroy ]
+    end
   end
 
-  namespace :admin do
-    root "top#index"
-    get "login" => "sessions#new", as: :login
-    post "session" => "sessions#create", as: :session
-    delete "session" =>  "sessions#destroy"
-    resources :staff_members
+  constraints host: config[:admin][:host] do
+    namespace :admin, path: config[:admin][:path] do
+      root "top#index"
+      get "login" => "sessions#new", as: :login
+      resource :session, only: [ :create, :destroy ]
+      resources :staff_members
+    end
   end
 
-  namespace :customer do
-    root "top#index"
+  constraints host: config[:customer][:host] do
+    namespace :customer, path: config[:customer][:path] do
+      root "top#index"
+    end
   end
-
 end
