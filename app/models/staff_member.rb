ee49be2d8e446b1e712ac307211e32fc3b902956
shoichinishiguchi <shoichi2228@yahoo.co.jp>
2020-03-01 09:57:40 +0900

    feat: 14-3enshuまで

--- app/models/staff_member.rb
+++ app/models/staff_member.rb
@@ -1,5 +1,35 @@
 class StaffMember < ApplicationRecord
+  include StringNormalizer
+
   has_many :events, class_name: "StaffEvent", dependent: :destroy
+
+  before_validation do
+    self.family_name = normalize_as_name(self.family_name)
+    self.given_name = normalize_as_name(self.given_name)
+    self.family_name_kana = normalize_as_furigana(self.family_name_kana)
+    self.given_name_kana = normalize_as_furigana(self.given_name_kana)
+    self.email = normalize_as_email(email)
+  end
+
+  HUMAN_NAME_REGXEP = /\A[\p{han}\p{hiragana}\p{katakana}\u{30fc}A-Za-z]+\z/
+  KATAKANA_REGEXP = /\A[\p{katakana}\u{30fc}]+\z/
+
+  validates :email, presence: true, "valid_email_2/email": true,
+    uniqueness: { case_sensitive: false }
+  validates :family_name, :given_name, presence: true,
+    format: { with: HUMAN_NAME_REGXEP, allow_blank: true }
+  validates :family_name_kana, :given_name_kana, presence: true,
+    format: { with: KATAKANA_REGEXP, allow_blank: true }
+  validates :start_date, presence: true, date: {
+    after_or_equal_to: Date.new(2000, 1, 1),
+    before: -> (obj) { 1.year.from_now.to_date },
+    allow_blank: true
+  }
+  validates :end_date, date: {
+    after: :start_date,
+    before: -> (obj) { 1.year.from_now.to_date },
+    allow_blank: true
+  }
 
   def password=(raw_password)
     if raw_password.kind_of?(String)
--- spec/models/staff_member_spec.rb
+++ spec/models/staff_member_spec.rb
@@ -14,6 +14,51 @@ RSpec.describe StaffMember, type: :model do
       member.password = nil
       expect(member.hashed_password).to be_nil
     end
+  end
+  describe "値の正規化" do
+    example "email前後の空白を削除" do
+      member = create(:staff_member, email: "test@example.com ")
+      expect(member.email).to eq("test@example.com")
+    end
+    example "email前後の全角スペースを削除" do
+      member = create(:staff_member, email: "　test@example.com　")
+      expect(member.email).to eq("test@example.com")
+    end
+    example "family_name_kanaに含まれるひらがなをカタカナに変換" do
+      member = create(:staff_member, family_name_kana: "てすと")
+      expect(member.family_name_kana).to eq("テスト")
+    end
+    example "family_name_kanaに含まれる半角カナを全角カタカナに変換" do
+      member = create(:staff_member, family_name_kana: "ﾃｽﾄ")
+      expect(member.family_name_kana).to eq("テスト")
+    end
+  end
+  describe "バリデーション" do
+    let!(:staff_member){ create(:staff_member, email: "test@example.com") }
+    let(:staff_member2){ build(:staff_member, email: "test@example.com") }
 
+    example "@を2こ含むものは無効" do
+      member = build(:staff_member, email: "test@@example.com")
+      expect(member).not_to be_valid
+    end
+    example "漢字を含むfamily_name_kanaは無効" do
+      member = build(:staff_member, family_name_kana: "カツ活")
+      expect(member).not_to be_valid
+    end
+    example "長音符を含むfamily_name_kanaは有効" do
+      member = build(:staff_member, family_name_kana: "エリー")
+      expect(member).to be_valid
+    end
+    example "漢字、ひながな、カタカナ、アルファベットを含むfamily_nameは有効" do
+      member = build(:staff_member, family_name: "太郎たろうタロウtaro")
+      expect(member).to be_valid
+    end
+    example "漢字、ひながな、カタカナ、アルファベット以外を含むfamily_nameは無効" do
+      member = build(:staff_member, family_name: "ファkjf＠w")
+      expect(member).not_to be_valid
+    end
+    example "同じemailをもつメンバーがいたら無効" do
+      expect(staff_member2).not_to be_valid
+    end
   end
 end
