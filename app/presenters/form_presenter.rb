04fcefa48e41b7cb20b36db05577de24146f7cfd
shoichinishiguchi <shoichi2228@yahoo.co.jp>
2020-03-29 19:05:52 +0900

    feat: 15-3まで

--- app/presenters/form_presenter.rb
+++ app/presenters/form_presenter.rb
@@ -7,4 +7,39 @@ class FormPresenter
     @form_builder = form_builder
     @view_context = view_context
   end
+
+  def notes
+    markup(:div, class: "notes") do |m|
+      m.span "*", class: "mark"
+      m.text "印のついた項目は入力必須です。"
+    end
+  end
+
+  def text_field_block(name, label_text, options = {})
+    markup(:div, class: "input-block") do |m|
+      m << decorated_label(name, label_text, options)
+      m << date_field(name, options)
+    end
+  end
+
+  def password_field_block(name, label_text, options = {})
+    markup(:div, class: "input-block") do |m|
+      m << decorated_label(name, label_text, options)
+      m << password_field(name, options)
+    end
+  end
+
+  def date_field_block(name, label_text, options = {})
+    markup(:div, class: "input-block") do |m|
+      m << decorated_label(name, label_text, options)
+      m << date_field(name, options)
+    end
+  end
+
+  private
+
+  def decorated_label(name, label_text, options = {})
+    label(name, label_text, class: options[:required] ? "required" : nil)
+  end
+
 end
